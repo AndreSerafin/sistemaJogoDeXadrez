5dc4822e6834524169ec0e542415db774127f567
Andre Serafin <andreserafim11@gmail.com>
2022-10-26 12:08:44 -0300

    cores no tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,6 +28,11 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
@@ -40,26 +45,43 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
+        System.out.println(" |________________________|");
+        int index = 0;
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((8 - i) + "|");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j],index);
+                index++;
             }
+            index++;
+            System.out.print("|");
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println(" |________________________|");
+        System.out.println("   A  B  C  D  E  F  G  H");
     }
 
-    private static void printPiece(ChessPiece piece) {
+    private static void printPiece(ChessPiece piece, int index) {
         if (piece == null){
-            System.out.print("-");
+            if(index % 2 == 0) {
+                System.out.print(ANSI_BLACK_BACKGROUND + " - " + ANSI_RESET);
+            }else {
+                System.out.print(ANSI_WHITE_BACKGROUND + " - " + ANSI_RESET);
+            }
         }else {
             if(piece.getColor() == Color.WHITE){
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+                if(index % 2 == 0) {
+                    System.out.print(ANSI_BLACK_BACKGROUND + " " + ANSI_RED + piece + " " + ANSI_RESET );
+                }else {
+                    System.out.print(ANSI_WHITE_BACKGROUND + " " + ANSI_RED + piece + " " + ANSI_RESET);
+                }
             }else{
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+                if(index % 2 == 0) {
+                    System.out.print(ANSI_BLACK_BACKGROUND + " " + ANSI_PURPLE + piece + " " + ANSI_RESET );
+                }else {
+                    System.out.print(ANSI_WHITE_BACKGROUND + " " + ANSI_PURPLE + piece + " " + ANSI_RESET);
+                }
             }
         }
-        System.out.print(" ");
     }
 }
